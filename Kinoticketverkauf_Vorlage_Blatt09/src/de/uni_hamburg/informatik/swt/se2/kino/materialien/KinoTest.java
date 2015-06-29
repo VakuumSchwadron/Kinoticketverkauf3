d338f5cdf84cab81aba7165d9115ff3acfcf7ee7
jannik <jannik@PC-Wyrnat>
2015-06-29 15:05:37 +0200

    Two beer or not two beer - that's the question! One does not simply change the constructor.

--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/KinoTest.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/KinoTest.java
@@ -3,12 +3,16 @@ package de.uni_hamburg.informatik.swt.se2.kino.materialien;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.FSK;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Geldbetrag;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Platz;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Uhrzeit;
 
@@ -33,6 +37,9 @@ public class KinoTest
     private Uhrzeit _u4;
 
     private Datum _d1;
+    
+    private Geldbetrag _g1;
+    private Geldbetrag _g2;
 
     private Vorstellung _vorstellungSaal1Film0;
     private Vorstellung _vorstellungSaal2Film1;
@@ -66,15 +73,18 @@ public class KinoTest
         _u4 = Uhrzeit.get(1, 30);
 
         _d1 = Datum.get(11, 07, 2008);
+        
+        _g1 = new Geldbetrag(900);
+        _g2 = new Geldbetrag(1000);
 
         _vorstellungSaal1Film0 = new Vorstellung(_saal1, _film0, _u1, _u2, _d1,
-                900);
+            _g1);
         _vorstellungSaal2Film1 = new Vorstellung(_saal2, _film1, _u1, _u2, _d1,
-                1000);
+                _g2);
         _vorstellungSaal2Film2a = new Vorstellung(_saal2, _film2, _u2, _u3,
-                _d1, 900);
+                _d1, _g1);
         _vorstellungSaal2Film2b = new Vorstellung(_saal2, _film2, _u3, _u4,
-                _d1, 900);
+                _d1, _g1);
 
         _alleVorstellungen = new Vorstellung[] { _vorstellungSaal1Film0,
                 _vorstellungSaal2Film1, _vorstellungSaal2Film2a,
