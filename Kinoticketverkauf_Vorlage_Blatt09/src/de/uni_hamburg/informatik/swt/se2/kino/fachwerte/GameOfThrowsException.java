50acb60e53fe87726bf7a32288370400d4a48a5b
barbieauglend <barbieauglend@icloud.com>
2015-07-06 15:01:16 +0200

    Yay!

--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GameOfThrowsException.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/GameOfThrowsException.java
@@ -2,7 +2,12 @@ package de.uni_hamburg.informatik.swt.se2.kino.fachwerte;
 
 public class GameOfThrowsException extends Exception
 {
-    public GameOfThrowsException(){super("Idiot! Betraege gibt man mit EE,CC an. Das weiss man doch!!");
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GameOfThrowsException(){super("Idiot! Betraege gibt man mit EE,CC an. Das weiss man doch!!");
     System.out.print(("Idiot! Betraege gibt man mit EE,CC an. Das weiss man doch!!"));}
     
 }
